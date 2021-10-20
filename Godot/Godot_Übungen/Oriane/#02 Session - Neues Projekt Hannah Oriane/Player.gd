3bbfbc140d730b6e1027044b7577759ba9d963f6
OrianeJoublin <92022973+OrianeJoublin@users.noreply.github.com>
2021-10-20 14:08:29 +0200

    Godot Übungsaufgabe
    Godot Übungsaufgabe

---
+++ Godot/Godot_Übungen/Oriane/#02 Session - Neues Projekt Hannah Oriane/Player.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
